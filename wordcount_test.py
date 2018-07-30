07b71bc40e93f415a7b61c81dc963adc3be4d8e0
pravinboppuri <30590150+pravinboppuri@users.noreply.github.com>
2018-07-30 19:24:13 +0530

    Update wordcount_test.py

--- wordcount_test.py
+++ wordcount_test.py
@@ -23,40 +23,34 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/bella/key.json"
 def main(argv=None):
   """Main entry point; defines and runs the wordcount pipeline."""
 
-  PROJECT = 'bigqueryproject-210006'
-  BUCKET = "gs://analytics_pravin"
+  PROJECT = 'project-id'
+  BUCKET = "cloud storage bucker"
 
   parser = argparse.ArgumentParser()
   parser.add_argument('--input',
                       dest='input',
-                      default='gs://tweetanalysis/Input/indiatrends.txt',
+                      default='input path',
                       help='Input file to process.')
   parser.add_argument('--output',
                       dest='output',
                       # CHANGE 1/5: The Google Cloud Storage path is required
                       # for outputting the results.
-                      default='gs://tweetanalysis/output',
+                      default='output path',
                       help='Output file to write results to.')
 
   known_args, pipeline_args = parser.parse_known_args(argv)
   pipeline_args.extend([
-      # CHANGE 2/5: (OPTIONAL) Change this to DataflowRunner to
-      # run your pipeline on the Google Cloud Dataflow Service.
+     
       '--runner=DataFlowRunner',
-      # CHANGE 3/5: Your project ID is required in order to run your pipeline on
-      # the Google Cloud Dataflow Service.
-      '--project=bigqueryproject-210006',
-      # CHANGE 4/5: Your Google Cloud Storage path is required for staging local
-      # files.
-      '--staging_location=gs://tweetanalysis/tweets',
-      # CHANGE 5/5: Your Google Cloud Storage path is required for temporary
-      # files.
-      '--temp_location=gs://tweetanalysis/TEMP',
+      
+      '--PROJECT',
+      '--staging_location=gs://somepath/tweets',
+      '--temp_location=gs://someptah/TEMP',
       '--job_name=tweet-wordcount-job',
   ])
 
-  # We use the save_main_session option because one or more DoFn's in this
-  # workflow rely on global context (e.g., a module imported at module level).
+ # pipeline creation
+
   pipeline_options = PipelineOptions(pipeline_args)
   pipeline_options.view_as(SetupOptions).save_main_session = True
   with beam.Pipeline(options=pipeline_options) as p:
@@ -83,7 +77,6 @@ def main(argv=None):
     output = counts | 'Format' >> beam.Map(format_result)
     #counts | 'Print' >> beam.ParDo(lambda (w, c): print('%s: %s' % (w, c)))
     # Write the output using a "Write" transform that has side effects.
-    # pylint: disable=expression-not-assigned
     output | WriteToText(known_args.output)
     #print(output)
 
