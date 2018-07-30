81a352fc938b481cf2cb51131c7d3fb8bc03f008
pravinboppuri <30590150+pravinboppuri@users.noreply.github.com>
2018-07-30 14:39:02 +0530

    Add files via upload
    using the bigquery client() to load data into bigquery table

---
+++ bigquery_load.py
@@ -0,0 +1,30 @@
+import os
+from google.cloud import bigquery
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/bella/key.json"
+bigquery_client = bigquery.Client()
+
+# from google.cloud import bigquery
+# client = bigquery.Client()
+filename = '/home/bella/PycharmProjects/TweetAnalysis/trend.csv'
+dataset_id = '123'
+table_id = 'trends'
+
+dataset_ref = bigquery_client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_id)
+job_config = bigquery.LoadJobConfig()
+job_config.source_format = bigquery.SourceFormat.CSV
+job_config.skip_leading_rows = 1
+job_config.autodetect = True
+
+with open(filename, 'rb') as source_file:
+    job = bigquery_client.load_table_from_file(
+        source_file,
+        table_ref,
+        location='US',  # Must match the destination dataset location.
+        job_config=job_config)  # API request
+
+job.result()  # Waits for table load to complete.
+
+print('Loaded {} rows into {}:{}.'.format(
+    job.output_rows, dataset_id, table_id))
